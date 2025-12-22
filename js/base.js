8066b9b427201ef0f5eb3092ae1b8483ce597bed
James Mitchell <mail@jpmitchell.ca>
2025-12-22 12:25:37 -0500

    [ 2025-12-22 ] theme list

--- js/base.js
+++ js/base.js
@@ -14,79 +14,121 @@ _paq.push(['enableLinkTracking']);
 
 /// Settings Popup
 
-// document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
 
-//     const footerButton = document.querySelector("footer-button"); 
-//     const bodyOverlay = document.querySelector("body-overlay"); 
-//     const bodyOverlayPopup = document.querySelector("body-overlay-popup");
-
-//     bodyOverlayPopup.addEventListener('click', () => { 
-
-//     }); 
-    
-//     bodyOverlay.addEventListener("click", (e) => { 
-//         if (e.target === bodyOverlay) { 
-//             bodyOverlay.style.display = "none"; 
-//         }
-//     });
-
-//     footerButton.addEventListener("click", () => {
-//         bodyOverlay.style.display = "flex"; 
-//     });
-// });
-
-/// Dark Mode 
-
- 
-
-
-function toggleDarkMode() { 
-    const attrDarkMode = document.body.getAttribute("data-dark-mode"); 
-    let dark_mode_value = localStorage.getItem(DARK_MODE_KEY) || "on"; 
-
-    if (attrDarkMode == null)
-        dark_mode_value = "on"; 
-    else if (attrDarkMode == "on")
-        dark_mode_value = "off"; 
-    else if (attrDarkMode == "off")
-        dark_mode_value = "on"; 
-
-    localStorage.setItem(DARK_MODE_KEY, dark_mode_value); 
-    document.body.setAttribute("data-dark-mode", dark_mode_value); 
-}
-
-document.addEventListener('DOMContentLoaded', () => { 
     const footerButton = document.querySelector("footer-button"); 
+    const popupButton = document.querySelector("body-overlay-popup-button"); 
+    const bodyOverlay = document.querySelector("body-overlay"); 
+    const bodyOverlayPopup = document.querySelector("body-overlay-popup");
 
-    const keyDarkMode = "data-dark-mode";
-
-    function toggleDarkMode(animateTransition) { 
-        const attrDarkMode = document.body.getAttribute(keyDarkMode); 
-        const valueDarkMode = 
-            attrDarkMode != null && attrDarkMode == "on" 
-            ? "off"
-            : "on";  
-
-        document.body.setAttribute(keyDarkMode, valueDarkMode); 
-        localStorage.setItem(keyDarkMode, valueDarkMode); 
-        
-        if (animateTransition) { 
-            document.body.classList.add('dark-mode-transition');
-                setTimeout(() => {
-                document.body.classList.remove('dark-mode-transition');
-            }, 300);
-        }
-        
+    function openPopup() { 
+        bodyOverlay.style.display = "flex"; 
     }
 
-    const attrDarkMode = document.body.getAttribute(keyDarkMode); 
-    const itemDarkMode = localStorage.getItem(keyDarkMode); 
-    if (attrDarkMode != itemDarkMode)
-        toggleDarkMode(false); 
+    function closePopup() { 
+        bodyOverlay.style.display = "none";
+    }
 
-    footerButton.addEventListener("click", (e) => { 
-        toggleDarkMode(true); 
+    // openPopup(); 
+
+    bodyOverlayPopup.addEventListener('click', () => { 
+
+    }); 
+    
+    bodyOverlay.addEventListener("click", (e) => { 
+        if (e.target === bodyOverlay) { 
+            closePopup(); 
+        }
     });
-    
-    
+
+    popupButton.addEventListener("click", (e) => { 
+        closePopup(); 
+    });
+
+    footerButton.addEventListener("click", () => {
+        openPopup();  
+    });
+});
+
+/// Theme List 
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    let pageHueIndex = parseInt(localStorage.getItem("theme-hue-index") ?? "0"); 
+    const themeItems = document.querySelectorAll("theme-item");
+
+    function updatePageHueIndex(idx) { 
+        pageHueIndex = idx; 
+        localStorage.setItem("theme-hue-index", idx); 
+        document.documentElement.style.setProperty("--color-primary-hue", 
+            themeItems[pageHueIndex].getAttribute("data-hue")
+        );
+    }
+
+    function updateThemeItems() { 
+        themeItems.forEach((it, idx) => {
+            it.setAttribute("data-active", idx == pageHueIndex ? "true" : "false"); 
+        });
+    }
+
+    themeItems.forEach((it, idx) => { 
+        it.addEventListener("click", (e) => { 
+            updatePageHueIndex(idx);      
+            updateThemeItems(); 
+        });
+    });
+
+    updateThemeItems();
+    updatePageHueIndex(pageHueIndex);
 }); 
+
+
+// function toggleDarkMode() { 
+//     const attrDarkMode = document.body.getAttribute("data-dark-mode"); 
+//     let dark_mode_value = localStorage.getItem(DARK_MODE_KEY) || "on"; 
+
+//     if (attrDarkMode == null)
+//         dark_mode_value = "on"; 
+//     else if (attrDarkMode == "on")
+//         dark_mode_value = "off"; 
+//     else if (attrDarkMode == "off")
+//         dark_mode_value = "on"; 
+
+//     localStorage.setItem(DARK_MODE_KEY, dark_mode_value); 
+//     document.body.setAttribute("data-dark-mode", dark_mode_value); 
+// }
+
+// document.addEventListener('DOMContentLoaded', () => { 
+//     const footerButton = document.querySelector("footer-button"); 
+
+//     const keyDarkMode = "data-dark-mode";
+
+//     function toggleDarkMode(animateTransition) { 
+//         const attrDarkMode = document.body.getAttribute(keyDarkMode); 
+//         const valueDarkMode = 
+//             attrDarkMode != null && attrDarkMode == "on" 
+//             ? "off"
+//             : "on";  
+
+//         document.body.setAttribute(keyDarkMode, valueDarkMode); 
+//         localStorage.setItem(keyDarkMode, valueDarkMode); 
+        
+//         if (animateTransition) { 
+//             document.body.classList.add('dark-mode-transition');
+//                 setTimeout(() => {
+//                 document.body.classList.remove('dark-mode-transition');
+//             }, 300);
+//         }
+        
+//     }
+
+//     const attrDarkMode = document.body.getAttribute(keyDarkMode); 
+//     const itemDarkMode = localStorage.getItem(keyDarkMode); 
+//     if (attrDarkMode != itemDarkMode)
+//         toggleDarkMode(false); 
+
+//     footerButton.addEventListener("click", (e) => { 
+//         toggleDarkMode(true); 
+//     });
+    
+// }); 
